afe910dc4413ae10899c494a506133f1d1308126
rutujadattajadhav <rutujadattajadhav@gmail.com>
2024-09-20 12:32:12 +0530

    initial commit

---
+++ src/main/java/com/dj/gateway/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.dj.gateway.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
